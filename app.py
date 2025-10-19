648e98a80ed3c86b00724c37d07e58a7555dbe17
DJCELL1 <selwyn.lopati@gmail.com>
2025-10-20 11:10:02 +1300

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ import pdfplumber
 import pytesseract
 import pdf2image
 import os
-from main 
-import extractor   # ✅ add this
+from main import extractor   # ✅ add this
 import openpyxl
 import pandas as pd
 import re
