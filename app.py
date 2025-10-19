5177cd13f242febb6ebb76686beaabe9d3f5baa7
DJCELL1 <selwyn.lopati@gmail.com>
2025-10-20 11:10:46 +1300

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ import streamlit as st
 import pdfplumber
 import pytesseract
 import pdf2image
-import os
-from main import extractor   # ✅ add this
+import os from main import extractor   # ✅ add this
 import openpyxl
 import pandas as pd
 import re
